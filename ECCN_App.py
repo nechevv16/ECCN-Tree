a8a02c40b4ac7924204b9c250d4407825e2c14bc
nechevv16 <nechevv16@gmail.com>
2024-01-27 14:03:42 -0700

    working

--- ECCN_App.py
+++ ECCN_App.py
@@ -1,23 +1,29 @@
+import customtkinter
+from PIL import Image
+import os
+
 class TitleFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
         self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
         self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+
 
 class MenuFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
+
 class TreeFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
+
 class ProgressFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
+
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -25,17 +31,21 @@ class App(customtkinter.CTk):
 
         self.title("Boom ECCN")
         self.geometry("600x1000")
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+
+        # configure grid layout (3x3)
+        self.grid_columnconfigure((0, 1, 2))
+        self.grid_rowconfigure((0, 1, 2))
+
 
         self.title_frame = TitleFrame(self)
-        self.title_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+        self.title_frame.grid(row=0, column=0, columnspan=3, padx=10, pady=(10, 0), sticky="nswe")
 
         self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
         self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew")
 
     def button_callback(self):
         print("button pressed")
+
 
 app = App()
 app.mainloop()
